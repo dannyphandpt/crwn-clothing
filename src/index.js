810dc5f046a8138a7702fc00b2b5ad3eda3a31e9
Danny Phan <dannyphandpt@gmail.com>
2019-08-19 03:26:33 -0400

    add routing and basic functions for buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const HatsPage = () => (
+    <div>
+        <h1> HATS PAGE </h1>
+    </div>
+);
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+);
